04004a7cd78cf2ead954c5d0fdd347d5dbd60c7f
vaxo <vaxojaniashvili186@gmail.com>
2024-02-22 02:01:51 +0400

    data-jsx-componnent

---
+++ src/components/born/date.jsx
@@ -0,0 +1,9 @@
+function Date (props) {
+    return (
+    <>
+    <h1 className="location">ვახო ჯანიაშვილი დაიბადა {props.location}</h1>
+    </>
+    )
+}
+
+export default Date;
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,13 +1,17 @@
 import "./main.css";
 import "../text/text.css";
-import logo from  "../../images/avatar.png";
+import logo from "../../images/avarat.jpg"
 import Name from "../text/title";
+import Date from "../born/date";
+
+
 function MainDiv() {
   return (
     <>
       <div className="mainDiv">
         <div className="top" style={{backgroundColor:"red", borderRadius:"40px"}}>
             <img className="avatar" src={logo} />
+            <Date location="Georgia"/>
         </div>
         <div className="bottom">
             <Name className="firstName" name="vaxo janiashvili"/>
