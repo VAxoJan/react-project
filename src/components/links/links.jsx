15151eb33acb1119620eab29aa7488cb2da1c63d
vaxo <vaxojaniashvili186@gmail.com>
2024-02-23 16:02:59 +0400

    link-fixed

---
+++ src/components/links/links.jsx
@@ -0,0 +1,6 @@
+export const HtmlLink = "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png";
+export const CSSLink = "https://cdn-icons-png.flaticon.com/512/919/919826.png";
+export const jsLink = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/768px-JavaScript-logo.png";
+export const ReactLink = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png";
+export const NextLink = "https://miro.medium.com/v2/resize:fit:1400/1*CStP06JrfLSevaOkPZavvg.png";
+export const BootstrapLink = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png";
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -5,6 +5,7 @@ import Date from "../born/date";
 import Profession from "../profession/profession";
 import Languages from "../languages/languages";
 import LangTitle from "../languages/languageTitle";
+import {HtmlLink, CSSLink, jsLink, ReactLink, NextLink, BootstrapLink} from "../links/links";
 
 function MainDiv() {
   return (
@@ -16,7 +17,7 @@ function MainDiv() {
             <Profession color="white" display="flex" justifyContent="center" fontWeight="400"/>
             <LangTitle display="flex" justifyContent="center" color="red" margin="10px 0"/>
             <div className="middle">
-              <Languages image="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png" width="40px" height="40px"/>
+              <Languages image={HtmlLink} width="40px" height="40px"/>
               <Languages image="https://cdn-icons-png.flaticon.com/512/919/919826.png" width="40px" height="40px"/>
               <Languages image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/768px-JavaScript-logo.png" width="40px" height="40px"/>
               <Languages image="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" width="40px" height="40px"/>
@@ -25,6 +26,7 @@ function MainDiv() {
               </div>
           </div>
         <div className="bottom">
+          <img className="botImg" src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHJqZTRvazR6ZWRvbXRkNHpzMmtyMDR0emY0bXVvM213cjBtYXM4dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/L8K62iTDkzGX6/giphy.gif"/>
         </div>
       </div>
     </>
