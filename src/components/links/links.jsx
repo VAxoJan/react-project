b9afc53089047203e3a1ea45663a01581f3047b1
vaxo <vaxojaniashvili186@gmail.com>
2024-02-23 16:04:30 +0400

    proect-finished

--- src/components/links/links.jsx
+++ src/components/links/links.jsx
@@ -4,3 +4,4 @@ export const jsLink = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a
 export const ReactLink = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png";
 export const NextLink = "https://miro.medium.com/v2/resize:fit:1400/1*CStP06JrfLSevaOkPZavvg.png";
 export const BootstrapLink = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png";
+export const gifImg = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHJqZTRvazR6ZWRvbXRkNHpzMmtyMDR0emY0bXVvM213cjBtYXM4dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/L8K62iTDkzGX6/giphy.gif";
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -5,7 +5,7 @@ import Date from "../born/date";
 import Profession from "../profession/profession";
 import Languages from "../languages/languages";
 import LangTitle from "../languages/languageTitle";
-import {HtmlLink, CSSLink, jsLink, ReactLink, NextLink, BootstrapLink} from "../links/links";
+import {HtmlLink, CSSLink, jsLink, ReactLink, NextLink, BootstrapLink, gifImg} from "../links/links";
 
 function MainDiv() {
   return (
@@ -18,15 +18,15 @@ function MainDiv() {
             <LangTitle display="flex" justifyContent="center" color="red" margin="10px 0"/>
             <div className="middle">
               <Languages image={HtmlLink} width="40px" height="40px"/>
-              <Languages image="https://cdn-icons-png.flaticon.com/512/919/919826.png" width="40px" height="40px"/>
-              <Languages image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/768px-JavaScript-logo.png" width="40px" height="40px"/>
-              <Languages image="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" width="40px" height="40px"/>
-              <Languages image="https://miro.medium.com/v2/resize:fit:1400/1*CStP06JrfLSevaOkPZavvg.png" width="40px" height="40px"/>
-              <Languages image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png" width="40px" height="40px"/>
+              <Languages image={CSSLink} width="40px" height="40px"/>
+              <Languages image={jsLink} width="40px" height="40px"/>
+              <Languages image={ReactLink} width="40px" height="40px"/>
+              <Languages image={NextLink} width="40px" height="40px"/>
+              <Languages image={BootstrapLink} width="40px" height="40px"/>
               </div>
           </div>
         <div className="bottom">
-          <img className="botImg" src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHJqZTRvazR6ZWRvbXRkNHpzMmtyMDR0emY0bXVvM213cjBtYXM4dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/L8K62iTDkzGX6/giphy.gif"/>
+          <img className="botImg" src={gifImg}/>
         </div>
       </div>
     </>
